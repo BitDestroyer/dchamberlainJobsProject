f7901b9592312586230a157491c678116512f182
Doug Chamberlain <dchamberlain12@gmail.com>
2020-02-04 01:35:39 -0500

    Added the automated tests

--- Job_List.py
+++ Job_List.py
@@ -4,11 +4,13 @@ from urllib import request
 # including the url and variable decleration
 url = "https://jobs.github.com/positions.json?&page=0"
 listOfJobs = []
+results = False
 
 
 # this function is to open a new file for storing data
 def new_file():
-    file = open("List Of Jobs", "w+")
+    file = open("List_Of_Jobs", "w+")
+    read_list(file)
     file.close()
 
 
@@ -16,13 +18,27 @@ def new_file():
 def read_list(file):
     for i in listOfJobs:
         i = i + " " + "\n"
-        print(i)
         file.write(i)
+
+
+def word_check():
+    for i in listOfJobs:
+        if i == "job":
+            results = True
 
 
 # this function is to grab the data from the url and make it a string
 def json_converter():
-    return str(urllib.request.urlopen(url).read().decode())
+    return str(urllib.request.urlopen(url).read().decode().split())
+
+
+def test_job_list_length():
+    assert len(listOfJobs) >= 100
+
+
+def test_List_Of_Jobs_contain():
+    word_check()
+    assert results == True
 
 
 # a classic main to run all the functions
