1590bf7aa836b721bedf4d9796c38023734c82ad
Doug Chamberlain <dchamberlain12@gmail.com>
2020-02-04 00:37:05 -0500

    Now with a proper working file saved with the json data and also comments

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import json
 import urllib
 from urllib import request
 
-#resp = request.urlopen("https://jobs.github.com/positions.json?page=0").read()
-#resp = json.dump(resp)
-#print(resp)
-
+# including the url and variable decleration
 url = "https://jobs.github.com/positions.json?&page=0"
-data = urllib.request.urlopen(url).read().decode()
-
-# parse json object
-obj = json.loads(data)
-
-
 listOfJobs = []
 
 
+# this function is to open a new file for storing data
 def new_file():
     file = open("List Of Jobs", "w+")
-    for i in listOfJobs:
-        i = i + " "
-        file.write(i)
     file.close()
 
 
+# this will read off the list of data in loop manor
+def read_list(file):
+    for i in listOfJobs:
+        i = i + " " + "\n"
+        print(i)
+        file.write(i)
+
+
+# this function is to grab the data from the url and make it a string
 def json_converter():
-    sitOfJobs = json.dump()
+    return str(urllib.request.urlopen(url).read().decode())
 
 
+# a classic main to run all the functions
 if __name__ == '__main__':
+    listOfJobs = [json_converter()]
     new_file()
+    # this print is for my own sanity to make sure the program works
+    print(listOfJobs)
