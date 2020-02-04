8d350ad534fdc102566860feee1b82c1f261ba70
Doug Chamberlain <dchamberlain12@gmail.com>
2020-02-03 23:42:29 -0500

    Have a working url json collector

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-listOfJobs = ["Butt", "Stuff", "butt stuff"]
+import json
+import urllib
+from urllib import request
+
+#resp = request.urlopen("https://jobs.github.com/positions.json?page=0").read()
+#resp = json.dump(resp)
+#print(resp)
+
+url = "https://jobs.github.com/positions.json?&page=0"
+data = urllib.request.urlopen(url).read().decode()
+
+# parse json object
+obj = json.loads(data)
+
+
+listOfJobs = []
 
 
 def new_file():
@@ -7,6 +22,10 @@ def new_file():
         i = i + " "
         file.write(i)
     file.close()
+
+
+def json_converter():
+    sitOfJobs = json.dump()
 
 
 if __name__ == '__main__':
