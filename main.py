91e2bf0b440aafffb639bf8677e8c7b6a966edfe
Doug Chamberlain <dchamberlain12@gmail.com>
2020-02-03 21:15:20 -0500

    First test in new project

---
+++ main.py
@@ -0,0 +1,13 @@
+listOfJobs = [1, 2, 3]
+
+
+def print_test():
+    print(listOfJobs)
+
+    #def new_file(self):
+     #   file = open(listOfJobs)
+     #   file.close()
+
+
+if __name__ == '__main__':
+    print_test()
