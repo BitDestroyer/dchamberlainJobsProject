04005847fe7f90d8d19371a1a9fe488792c7a0f4
Doug Chamberlain <dchamberlain12@gmail.com>
2020-03-11 17:54:40 -0400

    An updated version of sprint 3. Which now collects and stores into a DB a title and description.

--- Job_DB.py
+++ Job_DB.py
@@ -1,22 +1,40 @@
 import Job_List
 import sqlite3
 from typing import Tuple
-
-def open_db(filename:str)->Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    db = sqlite3.connect(filename)
-    cursor = db.cursor()
-    return db,cursor
+import xml.etree.ElementTree as ET
 
 
+def open_db(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    connection = sqlite3.connect(filename)
+    return connection
 
-def setup_db(cursor:sqlite3.Cursor):
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS job_title(job_name Text)''')
+def setup_db(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS jobs(title TEXT, description TEXT);''')
 
 
-def job_list(cursor:sqlite3.Cursor):
-    true_list = []
-    #for i in range(number of jobs()
-    # add a job name in a column in the database
-    for item in true_list:
-       cursor.execute(f'''INSERT INTO job_title(job_name) VALUES(?)''', (str(item),))
+def store_in_db(cursor, title, description):
+    cursor.execute(f'''INSERT INTO jobs (title, description) VALUES (?, ?)''', (str(title),str(description),))
+
+
+def parse_xml(cursor, xmlfile):
+    # create element tree object
+    tree = ET.parse(xmlfile)
+
+    # get root element
+    root = tree.getroot()
+
+    # iterate news items
+    for item in root.findall('./channel/item'):
+        title = item.find('./title')
+        description = item.find('./description')
+        store_in_db(cursor, title.text, description.text)
+
+
+con = open_db("jobs.db")
+cur = con.cursor()
+setup_db(cur)
+
+parse_xml(cur, 'list_of_jobs.xml')
+
+con.commit()
